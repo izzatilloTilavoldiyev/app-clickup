173e59e31110d2a262a5b1a15414860558af860a
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-06-18 21:26:11 +0500

    security configuration committed

---
+++ src/main/java/uz/pdp/appclickup/entity/Workspace.java
@@ -0,0 +1,31 @@
+package uz.pdp.appclickup.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import uz.pdp.appclickup.entity.template.AbsLongEntity;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Workspace extends AbsLongEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User owner;
+
+    @Column(nullable = false)
+    private String initialLetter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Attachment avatar;
+
+}
---
+++ src/main/java/uz/pdp/appclickup/entity/WorkspaceRole.java
@@ -0,0 +1,26 @@
+package uz.pdp.appclickup.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import uz.pdp.appclickup.entity.enums.WorkspaceRoleName;
+import uz.pdp.appclickup.entity.template.AbsUUIDEntity;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class WorkspaceRole extends AbsUUIDEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Workspace workspace;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private WorkspaceRoleName extendsRole;
+}
