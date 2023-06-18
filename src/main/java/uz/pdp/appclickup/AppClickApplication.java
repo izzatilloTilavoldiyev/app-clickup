5b8ed764e8cf811cc990fb0b515015fe96b8133b
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-06-18 21:22:15 +0500

    project created

---
+++ src/main/java/uz/pdp/appclickup/AppClickApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appclickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClickApplication.class, args);
+    }
+
+}
