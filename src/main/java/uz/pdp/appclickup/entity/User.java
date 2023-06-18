ec8b1175191748d7725a286a0b3aa6e65413ebdf
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-06-18 21:26:11 +0500

    entities committed

---
+++ src/main/java/uz/pdp/appclickup/entity/User.java
@@ -0,0 +1,79 @@
+package uz.pdp.appclickup.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import uz.pdp.appclickup.entity.enums.SystemRoleName;
+import uz.pdp.appclickup.entity.template.AbsUUIDEntity;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class User extends AbsUUIDEntity implements UserDetails {
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String color;
+
+    private String initialLetter;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Attachment avatar;
+    @Enumerated
+    private SystemRoleName systemRoleName;
+
+    private String emailCode;
+
+    public User(String fullName, String email, String password, SystemRoleName systemRoleName) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.systemRoleName = systemRoleName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(this.systemRoleName.name());
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
